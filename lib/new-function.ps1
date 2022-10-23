bf6dcd40f32340084b358573669db3e71ab95652
MicrowavableMuffin <68025484+MicrowavableMuffin@users.noreply.github.com>
2022-10-23 17:24:09 -0600

    Update new-function.ps1

--- lib/new-function.ps1
+++ lib/new-function.ps1
@@ -1,5 +1,5 @@
 # Write quick-scrit : 1666540007
-function New-Function([String]$Name) {
+function New-Function([Parameter(Mandatory=$true)][String]$Name) {
   if($env:editor) { $editor = $env:editor } else { $editor = 'notepad' }
   & $editor $env:UserProfile\scrit\$Name
   Restart-Shell
